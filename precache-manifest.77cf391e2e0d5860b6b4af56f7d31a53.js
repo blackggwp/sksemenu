280d95af5c52e3429a7028720f303ab8401d9552
blackMBP <you@example.com>
2020-05-28 09:36:46 +0700

    Updates

---
+++ precache-manifest.77cf391e2e0d5860b6b4af56f7d31a53.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7ad3b6cbf465417a04d1a1babbf94b7",
+    "url": "/sksemenu/index.html"
+  },
+  {
+    "revision": "e6249387ca5a57523851",
+    "url": "/sksemenu/static/css/2.367471c6.chunk.css"
+  },
+  {
+    "revision": "44631e9053d9936ea738",
+    "url": "/sksemenu/static/css/main.89a3be81.chunk.css"
+  },
+  {
+    "revision": "e6249387ca5a57523851",
+    "url": "/sksemenu/static/js/2.ecb0a9a1.chunk.js"
+  },
+  {
+    "revision": "44631e9053d9936ea738",
+    "url": "/sksemenu/static/js/main.04ac3510.chunk.js"
+  },
+  {
+    "revision": "9f344ae501f7fa6fd601",
+    "url": "/sksemenu/static/js/runtime~main.6d42704a.js"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/sksemenu/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/sksemenu/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "a6f24e7dd5470a8bd2f48ebb53dbe8d5",
+    "url": "/sksemenu/static/media/damir-bosnjak.a6f24e7d.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/sksemenu/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sksemenu/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aab414f7b69d0d4ad897e75e735f240a",
+    "url": "/sksemenu/static/media/mike.aab414f7.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/sksemenu/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/sksemenu/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/sksemenu/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/sksemenu/static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);
