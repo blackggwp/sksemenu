690d49e00154f826b272df748239a9414f565e85
black <ironhighh@gmail.com>
2021-03-18 14:11:01 +0700

    adjust layout for renderPage

--- src/layouts/Manual.jsx
+++ src/layouts/Manual.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { useParams } from "react-router-dom";
 import { Document, Page, pdfjs } from "react-pdf";
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+// import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -11,6 +13,18 @@ const options = {
 };
 
 export default function Manual() {
+
+  return (
+    <>
+      <CssBaseline />
+      <Container maxWidth="xl">
+        <RenderPdf />
+      </Container>
+    </>
+  );
+}
+
+function RenderPdf() {
   const [numPages, setNumPages] = useState(null);
   let { manualId } = useParams();
 
@@ -19,31 +33,28 @@ export default function Manual() {
   }
 
   return (
-    <div className="Example">
+    <div style={{ textAlign: '-webkit-center' }}>
       <header>
         <h1>{`${manualId}`.toUpperCase()}</h1>
       </header>
-      <div className="Example__container">
-
-        <div className="Example__container__document">
-          <Document
-            file={`/manual/${manualId}.pdf`}
-            onLoadSuccess={onDocumentLoadSuccess}
-            options={options}
-          >
-            {
-              Array.from(
-                new Array(numPages),
-                (el, index) => (
-                  <Page
-                    key={`page_${index + 1}`}
-                    pageNumber={index + 1}
-                  />
-                ),
-              )
-            }
-          </Document>
-        </div>
+      <div className="Example__container__document">
+        <Document
+          file={`/manual/${manualId}.pdf`}
+          onLoadSuccess={onDocumentLoadSuccess}
+          options={options}
+        >
+          {
+            Array.from(
+              new Array(numPages),
+              (el, index) => (
+                <Page
+                  key={`page_${index + 1}`}
+                  pageNumber={index + 1}
+                />
+              ),
+            )
+          }
+        </Document>
       </div>
     </div>
   );
