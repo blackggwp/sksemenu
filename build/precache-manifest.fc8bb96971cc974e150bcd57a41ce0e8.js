d48ccbf1114bb60f1d1e2148d52cd6bc368631c5
black <ironhighh@gmail.com>
2021-03-25 13:46:26 +0700

    expose build folder

---
+++ build/precache-manifest.fc8bb96971cc974e150bcd57a41ce0e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1121c8e31f7759068476529ea7922126",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aef3b1c93d9191edb65d",
+    "url": "/static/css/2.58574396.chunk.css"
+  },
+  {
+    "revision": "aef3b1c93d9191edb65d",
+    "url": "/static/js/2.61927b66.chunk.js"
+  },
+  {
+    "revision": "a70560720fba7c7e5b3b3be8279c920d",
+    "url": "/static/js/2.61927b66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eca55eb49050619a283f",
+    "url": "/static/js/main.50057d1b.chunk.js"
+  },
+  {
+    "revision": "1ea9d29fcd9c0181a644",
+    "url": "/static/js/runtime-main.95477324.js"
+  }
+]);
