f883595476b6e12df0c0a37ec331a9ff90869765
blackggwp <ironhighh@gmail.com>
2021-06-17 12:00:09 +0700

    fix db-helvethaica font

--- src/themes/index.js
+++ src/themes/index.js
@@ -1,6 +1,20 @@
 import defaultTheme from "./default";
-
 import { createMuiTheme, responsiveFontSizes } from "@material-ui/core";
+import dbhelvethaicamon from "../assets/fonts/db-helvethaicamon.ttf";
+
+const helvethaicamon = {
+  fontFamily: "dbhelvethaicamon",
+  fontStyle: "normal",
+  fontDisplay: "swap",
+  fontWeight: 400,
+  src: `
+      local('dbhelvethaicamon'),
+      local('dbhelvethaicamon-Regular'),
+      url(${dbhelvethaicamon}) format('ttf')
+    `,
+  unicodeRange:
+    "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF",
+};
 
 const overrides = {
   typography: {
@@ -25,12 +39,14 @@ const overrides = {
     caption: {
       fontSize: "1.5rem",
     },
-    fontFamily: [
-      '"Helvetica Neue"',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(","),
+    fontFamily: "dbhelvethaicamon, Arial",
+    overrides: {
+      MuiCssBaseline: {
+        "@global": {
+          "@font-face": [helvethaicamon],
+        },
+      },
+    },
   },
 };
 
